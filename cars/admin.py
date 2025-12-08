ae5e531a39fbcb469a74ae8902631118075bca26
GeorgeKemmerich <georgekemmerich@gmail.com>
2025-12-08 14:40:29 -0300

    Primeiro Commit do projeto usando django e SQLite, foi criado as duas primeiras Relaçoes do banco de dados (Cars PK e Brand FK)

---
+++ cars/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from cars.models import Car
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'price')
+    search_fields = ('model', 'model')
+
+    
+admin.site.register(Car, CarAdmin)
---
+++ cars/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    
+    
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200, blank=True, null=True)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
+    version = models.CharField(max_length=150)
+    motor = models.CharField(max_length=15, null=True)
+    category = models.CharField(max_length=150, null=True)
+    transmission = models.CharField(max_length=100)
+    fuel_type = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    factory_year = models.IntegerField()
+    model_year = models.IntegerField()
+    price = models.FloatField(blank=True, null=True)
+    state = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.model
+        
