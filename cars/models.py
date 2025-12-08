8dc618859dab17fda5e5430674ccc61653cbcf03
GeorgeKemmerich <georgekemmerich@gmail.com>
2025-12-08 14:52:56 -0300

    makemigrate model Brand FK

--- cars/models.py
+++ cars/models.py
@@ -5,6 +5,7 @@ class Brand(models.Model):
     name = models.CharField(max_length=100)
     
     
+    
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200, blank=True, null=True)
