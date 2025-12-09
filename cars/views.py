0ca82d707ab1441630af5c2ec09aeab2f8442668
GeorgeKemmerich <georgekemmerich@gmail.com>
2025-12-09 05:04:58 -0300

    Primeira alteracao de CBV (CarsView), assim perdemos a def cars_view feita com FBV

--- cars/views.py
+++ cars/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import redirect, render
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 # Create your views here.
 
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
+class CarsView(View):
 
-    if search:
-        cars = Car.objects.filter(model__icontains=search).order_by('model')
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        search = request.GET.get('search')
+
+        if search:
+            cars = Car.objects.filter(model__icontains=search).order_by('model')
     
-    return render(
+        return render(
         request, 
         'cars.html',
           {'cars': cars }
- )
+    )
 
 def new_car_view(request):
     if request.method == 'POST':
