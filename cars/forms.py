5c32628dbbedb93bf739308cfc9afa151c491392
GeorgeKemmerich <georgekemmerich@gmail.com>
2025-12-09 01:05:47 -0300

    Excluindo o CarForm e usando o CarModelForm

--- cars/forms.py
+++ cars/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from cars.models import Brand, Car
+from cars.models import Car
 
 class CarModelForm(forms.ModelForm):
         class Meta:
              model = Car
              fields = '__all__'
+
+        def clean_price(self):
+               price = self.cleaned_data.get('price') 
+               if price < 8000:
+                self.add_error('price', 'Valor Mínimo R$8,000!')
+                return price
