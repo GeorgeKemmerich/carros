e228e76ac072381b8c7e22b1f7186b6465e101f5
GeorgeKemmerich <georgekemmerich@gmail.com>
2025-12-08 23:41:49 -0300

    Criado a funcao save manualmente em forms.py

--- cars/forms.py
+++ cars/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Brand, Car
 
 class CarForm(forms.Form):
     model = forms.CharField()
@@ -15,3 +15,22 @@ class CarForm(forms.Form):
     price = forms.FloatField()
     state = forms.CharField()
     photo = forms.ImageField()
+
+    def save(self):
+        car = Car(
+            model = self.cleaned_data['model'],
+            brand = self.cleaned_data['brand'],
+            version = self.cleaned_data['version'],
+            motor = self.cleaned_data['motor'],
+            category = self.cleaned_data['category'],
+            transmission = self.cleaned_data['transmission'],
+            fuel_type = self.cleaned_data['fuel_type'],
+            color = self.cleaned_data['color'],
+            factory_year = self.cleaned_data['factory_year'],
+            model_year = self.cleaned_data['model_year'],
+            price = self.cleaned_data['price'],
+            state = self.cleaned_data['state'],
+            photo = self.cleaned_data['photo'],
+        )
+        car.save()  
+        return car
