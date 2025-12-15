ce0df70501c327d85431db94b12614108d2b304e
GeorgeKemmerich <georgekemmerich@gmail.com>
2025-12-15 20:26:13 -0300

    Alterando psycopg2 para binary

--- app/settings.py
+++ app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carros',
+        'USER': 'postgres',
+        'PASSWORD': 'Idmlixo2017',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -121,6 +125,5 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# No final do arquivo settings.py
 
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
